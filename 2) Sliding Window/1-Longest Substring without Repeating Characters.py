e7d55342c0f484e8a7f185864bdec76d3f237263
Pavan-Rajana <pkcoder.res@gmail.com>
2024-10-15 17:39:02 +0530

    Longest SubString

--- 2) Sliding Window/1-Longest Substring without Repeating Characters.py
+++ 2) Sliding Window/1-Longest Substring without Repeating Characters.py
@@ -26,20 +26,20 @@ abs = "abcabcbb"
 sol = Solution()
 print(sol.lengthOfLongestSubstring(abs))    
 
-# ? Example: s = "abcabcbb"
+# ! Example: s = "abcabcbb"
 """
-\\Third iteration (r = 2):
+# ? Third iteration (r = 2):
 s[2] = 'c', which is not in sett.
 Add 'c' to sett: sett = {'a', 'b', 'c'}.
 Window length w = (2 - 0) + 1 = 3, longest = max(2, 3) = 3.
 
-\\Fourth iteration (r = 3):
+# ? Fourth iteration (r = 3):
 s[3] = 'a', which is in sett (repeat detected).
 Remove s[0] = 'a' from sett: sett = {'b', 'c'}, and move l = 1.
 Add s[3] = 'a' to sett: sett = {'a', 'b', 'c'}.
 Window length w = (3 - 1) + 1 = 3, longest = max(3, 3) = 3.
 
-\\ Fifth iteration (r = 4):
+# ? Fifth iteration (r = 4):
 s[4] = 'b', which is in sett (repeat detected).
 Remove s[1] = 'b' from sett: sett = {'a', 'c'}, and move l = 2.
 Add s[4] = 'b' to sett: sett = {'a', 'b', 'c'}.
